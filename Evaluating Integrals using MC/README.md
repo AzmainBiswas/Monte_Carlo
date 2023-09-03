0aa3b4443e32c2dd46aa8bd2b2886841a62ccac7
AzmainBiswas <azmainbiswas007@gmail.com>
2023-09-03 20:59:09 +0530

    fist commit

---
+++ Evaluating Integrals using MC/README.md
@@ -0,0 +1,11 @@
+# Evaluating itegrals using Monte Carlo method (MC)
+## Example
+Evaluate the following using MC simulation
+$$
+    \int_0^1 e^{-\frac{x^2}{2}} dx
+$$
+
+## Result
+Value of integration (Sample Expection) = 0.8556222637805251
+
+![result](../asste/Int_MC_His.png)
---
+++ Evaluating Integrals using MC/main.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+S = 1000  # number of simulations
+I = 1000  # numbre of iterations
+A = 0.0  # upper limit
+B = 1  # lower limit
+
+def fun(x):
+  return np.exp(-(x**2)/2)
+
+Integrations = []
+for i in range(S):
+  Sum = 0
+  for i in range(I):
+    x = np.random.uniform(0,1)
+    mod_fun = fun((B-A)*x+A)*(B-A)
+    Sum += mod_fun
+
+  Int = Sum/float(I)
+  Integrations.append(Int)
+
+sum = 0
+for i in Integrations:
+    sum += i
+sample_expection = sum/float(S)
+
+print("")
+print(f'Sample Expection: {sample_expection}')
+
+plt.title("Evaluating Integral")
+plt.grid()
+plt.hist(Integrations,bins=50, edgecolor='#000000')
+plt.show()
